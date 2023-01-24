0b80de2b83ef3f1c4da242dc8e745a98caae2ba6
Witold Matusiński <matusinski@interia.pl>
2023-01-24 15:45:14 +0100

    Initial commit

---
+++ src/main/java/pl/edu/wszib/jwdcolors/JwdColorsApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwdcolors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwdColorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwdColorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/jwdcolors/JwdColorsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwdcolors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwdColorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
